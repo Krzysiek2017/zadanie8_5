0fe4673df9fe0fc07a7cc241371093cc501353da
Logan <musashimiyamoto@interia.eu>
2017-08-21 00:36:27 +0200

    poprawka do uwagi

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 20, prompt('Wpisz wartość podstawy trójkąta a'),
-    h = 8, prompt('Wpisz wartość wysokości trójkąta h'),
+var a = prompt('Wpisz wartość podstawy trójkąta a'),
+    h = prompt('Wpisz wartość wysokości trójkąta h'),
     triangleArea = a * h / 2;
 alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 console.log('Triangle field with base a:' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
